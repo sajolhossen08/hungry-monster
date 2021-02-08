f09bf4c4ffc9696e3a07253e8c01a54bcd51ed0d
Shozol Hossen <khansajol08@gmail.com>
2021-02-08 14:37:22 +0600

    almost finished

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
-const mealDetails = document.getElementById('meal-details');
+
 
 // event listeners
 searchBtn.addEventListener('click', getMealList);
@@ -34,22 +34,54 @@ function getMealList(){
         mealList.innerHTML = html;
     });
 }
+
+
+
+
+
+
+
+
+
+
 function mealDetailsView(id){
-    console.log(id);
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
+        displayMealDetails(data.meals);
 
-        mealDetails.classList.remove('d-none');
-
-        const htmlTemplate = `
-        <div>
-        <img src = "${meal.strMealThumb}" alt = "">
-        </div>
-        <h2>${meal.strMeal}</h2>
-        `
-        mealDetails.innerHTML = htmlTemplate;
     })
 }
+const displayMealDetails = meals => {
+    console.log(meals);
+    const mealDetails = document.getElementById('meal-details');
+    let html = "";
+    meals.forEach(meal => {
 
+        html += `
+            <div class = "meal-details-img">
+                <img src = "${meal.strMealThumb}" alt = "food">
+            </div>
+            <div class = "meal-details-name">
+                <h3>${meal.strMeal}</h3>
+                <h6>Origin area : ${meal.strArea}</h6>
+                <h6>Food type : ${meal.strTags}</h6>
+            </div>
+            <div class = "list-of-recipes">
+                <h5>Ingredients :</h5>
+                <ul class="ingredients">
+                    <li>${meal.strMeasure1} ${meal.strIngredient1}</li>
+                    <li>${meal.strMeasure2} ${meal.strIngredient2}</li>
+                    <li>${meal.strMeasure3} ${meal.strIngredient3}</li>
+                    <li>${meal.strMeasure4} ${meal.strIngredient4}</li>
+                    <li>${meal.strMeasure5} ${meal.strIngredient5}</li>
+                    <li>${meal.strMeasure6} ${meal.strIngredient6}</li>
+                </ul>
+            </div>
+            <hr>
+    `;
+        mealDetails.innerHTML = html;
+
+        mealDetails.classList.remove('d-none');
+    })
+}
